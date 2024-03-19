58a43a02feae6b18fe34c69564167085bb9809a7
Gustavo Nunes <gustavompn06@gmail.com>
2024-03-19 14:32:31 -0300

    project push

---
+++ SistemaDeTarefas/Enums/StatusTarefa.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+
+namespace SistemaDeTarefas.Enums
+{
+    public enum StatusTarefa
+    {
+
+        [Description("A fazer")]
+        Afazer = 1,
+        [Description("Em andamento")]
+        EmAndamento = 2,
+        [Description("Concluido")]
+        Concluido = 3
+
+    }
+}
